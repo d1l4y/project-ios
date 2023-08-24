ba51426d1514c52150d9147f979e8219df166caf
d1l4y <viniciusdilay@protonmail.com>
2023-08-23 22:24:35 -0300

    add xcode project and removes storyboard

---
+++ Verity iOS/Verity iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Verity iOS
+//
+//  Created by Vinicius Augusto Dilay de Paula on 23/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
